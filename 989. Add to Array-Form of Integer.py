ca99910cd5844c2a306f1634f2073e765abcbf92
obsu kebede <134233644+ob971@users.noreply.github.com>
2023-12-28 11:56:46 +0300

    989. Add to Array-Form of Integer.py

---
+++ 989. Add to Array-Form of Integer.py
@@ -0,0 +1,4 @@
+sys.set_int_max_str_digits(10**6)
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        return list(map(int,str(int("".join(map(str,num))) + k)))
